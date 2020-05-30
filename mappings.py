d6ba65aa4e1034c644a1c27f29a19c00a8030660
KenHBS <kenschroeder@babbel.com>
2020-05-30 22:59:46 +0200

    add mappings

---
+++ mappings.py
@@ -0,0 +1,66 @@
+twitter_mapping = {
+    'a': {
+        'name': 'Barack Obama',
+        'value': 117
+    },
+    'b': {
+        'name': 'Bill Gates',
+        'value': 50
+    },
+    'c': {
+        'name': 'Cristiano Ronaldo',
+        'value': 84
+    },
+    'd': {
+        'name': 'Donald Trump',
+        'value': 79
+    },
+    'e': {
+        'name': 'Kim Kardashian',
+        'value': 65
+    },
+    'f': {
+        'name': 'Narendra Modi',
+        'value': 56
+    }
+}
+
+
+release_mapping = {
+    'a': {
+        'name': 'Britney Spears',
+        'value': 1998
+    },
+    'b': {
+        'name': 'Dolly clone',
+        'value': 1996
+    },
+    'c': {
+        'name': 'FIFA',
+        'value': 1993
+    },
+    'd': {
+        'name': 'iPod',
+        'value': 2001
+    },
+    'e': {
+        'name': 'Nintendo GameBoy',
+        'value': 1989
+    },
+    'f': {
+        'name': 'Nokia 3310',
+        'value': 2000
+    },
+    'g': {
+        'name': 'SQL database lanuage',
+        'value': 1974
+    },
+    'h': {
+        'name': 'Sony Discman',
+        'value': 1984
+    },
+    'i': {
+        'name': 'Windows 1.0',
+        'value': 1985
+    }
+}
